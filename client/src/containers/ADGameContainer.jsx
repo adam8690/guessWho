9bc0d704c19582407a316d7cc472c1f2fd0b8784
Adam <adam8690@hotmail.co.uk>
2017-05-07 16:39:56 +0100

    added questions object and passed some items down as props.

--- client/src/containers/ADGameContainer.jsx
+++ client/src/containers/ADGameContainer.jsx
@@ -11,6 +11,14 @@ constructor(props){
         {name: "Michael Bluth", hairColour: "Brown", hasHair:true, eyes: "Blue", glasses:false, gender:"male", surname:"Bluth", img: "http://cdn2us.denofgeek.com/sites/denofgeekus/files/jasonbateman_michael.png"},
         {name: "Gob Bluth", hairColour: "Brown", hasHair:true, eyes: "Brown", glasses:false, gender:"male", surname:"Bluth", img: "http://bloximages.newyork1.vip.townnews.com/stltoday.com/content/tncms/assets/v3/editorial/b/e0/be024d5c-07f3-5153-b1cf-bc1e67cf5dd5/519fb7561dd53.preview-620.jpg"}
     ]
+    this.selectedCard = this.cards[0]
+    this.questions = [
+        { "Is their hair": ["Brown", "Black", "Blonde"] },
+        { "Do they have": ["Hair", "Glasses"] },
+        { "Are their eyes": ["Brown", "Blue"] },
+        { "Are they": ["Male", "Female"] },
+        { "Is their Surname": ["Bluth", "Funke"] },
+    ]
 }
 
 render(){
@@ -18,8 +26,8 @@ render(){
         <div id="Game">
             <h2>Arrested Development Edition</h2>
             <CardsContainer cards={this.cards}/>
-            <QAContainer />
-            <GuessContainer />
+            <QAContainer questions={this.questions}/>
+            <GuessContainer cards={this.cards}/>
         </div>
     )
 }
