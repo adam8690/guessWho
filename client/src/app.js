40bf1366de7aebdecb5220324e0d2c3d01d8afa4
Adam <adam8690@hotmail.co.uk>
2017-05-07 12:52:09 +0100

    Initial commit, file structure created and title displayed

---
+++ client/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ADGameContainer from './containers/ADGameContainer'
+
+window.onload = function(){
+  ReactDOM.render(
+    <div>
+      <h1>Guess Who?</h1>
+      <ADGameContainer />
+    </div>,
+    document.getElementById('app')
+  );
+}
---
+++ client/src/containers/ADGameContainer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+// import CardsContainer from './CardsContainer'
+// import QAContainer from './QAContainer'
+// import GuessContainer from './GuessContainer'
+
+class ADGameContainer extends React.Component{
+
+
+render(){
+    return (
+        <div id="Game">
+            <h2>Arrested Development Edition</h2>
+            {/*<CardsContainer />
+            <QAContainer />
+            <GuessContainer />*/}
+        </div>
+    )
+}
+
+}
+
+export default ADGameContainer
