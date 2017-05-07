cab2ab879fd5579aa16bc2a88a36e5569dd22d9b
Adam <adam8690@hotmail.co.uk>
2017-05-07 16:19:16 +0100

    added extra data to card objects

--- client/src/containers/ADGameContainer.jsx
+++ client/src/containers/ADGameContainer.jsx
@@ -8,8 +8,8 @@ class ADGameContainer extends React.Component{
 constructor(props){
     super(props)
     this.cards = [
-        {name: "Michael Bluth", hairColour: "Brown", hasHair:"yes", eyes: "Blue", img: "http://cdn2us.denofgeek.com/sites/denofgeekus/files/jasonbateman_michael.png"},
-        {name: "Gob Bluth", hairColour: "Brown", hasHair:"yes", eyes: "Brown", img: "http://bloximages.newyork1.vip.townnews.com/stltoday.com/content/tncms/assets/v3/editorial/b/e0/be024d5c-07f3-5153-b1cf-bc1e67cf5dd5/519fb7561dd53.preview-620.jpg"}
+        {name: "Michael Bluth", hairColour: "Brown", hasHair:true, eyes: "Blue", glasses:false, gender:"male", surname:"Bluth", img: "http://cdn2us.denofgeek.com/sites/denofgeekus/files/jasonbateman_michael.png"},
+        {name: "Gob Bluth", hairColour: "Brown", hasHair:true, eyes: "Brown", glasses:false, gender:"male", surname:"Bluth", img: "http://bloximages.newyork1.vip.townnews.com/stltoday.com/content/tncms/assets/v3/editorial/b/e0/be024d5c-07f3-5153-b1cf-bc1e67cf5dd5/519fb7561dd53.preview-620.jpg"}
     ]
 }
 
